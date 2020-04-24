80299c05f8b81ca8e1a1f8520a99938cae16d955
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-04-24 17:10:01 -0300

    Add files via upload

---
+++ Listando agenda do prestador/src/routes.js
@@ -0,0 +1,34 @@
+import {Router}from'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import FileController from './app/controllers/FileController'
+import authMiddleware from './app/middlewares/auth'
+import ProviderController from './app/controllers/ProviderController'
+import ApointmentController from './app/controllers/AppointmentController'
+import ScheduleController from './app/controllers/ScheduleController';
+
+const routes=new Router();
+const upload=multer(multerConfig);
+
+routes.post('/users',UserController.store);
+routes.post('/sessions',SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users',UserController.update);
+
+routes.get('/providers',ProviderController.index)
+
+routes.post('/files',upload.single('file'),FileController.store);
+
+
+
+
+routes.post('/appointments',ApointmentController.store);
+routes.get('/appointments',ApointmentController.index);
+
+routes.get('/schedule',ScheduleController.index);
+
+
+export default routes;
