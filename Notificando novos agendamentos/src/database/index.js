62fc79af36a5cb42d625841d7c1136de06588959
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-04-24 21:40:33 -0300

    Add files via upload

---
+++ Notificando novos agendamentos/src/database/index.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+import User from '../app/models/User';
+import File from '../app/models/File';
+import databaseConfig from '../config/database';
+import Appointment from '../app/models/Appointment'
+import mongoose  from 'mongoose';
+
+const models=[User,File,Appointment];
+
+class Database{
+    constructor(){
+        this.init();
+        this.mongo();
+    }
+    init(){
+        this.connection=new Sequelize(databaseConfig);
+        models.map(model=>model.init(this.connection)).map(model=>model.associate&&model.associate(this.connection.models));
+    }
+    mongo(){
+        this.mongoConnection=mongoose.connect(
+            'mongodb://localhost:27017/gobarber',
+            { useNewUrlParser:true,useFindAndModify:true ,useUnifiedTopology: true}
+        )
+    }
+}
+export default new Database();
