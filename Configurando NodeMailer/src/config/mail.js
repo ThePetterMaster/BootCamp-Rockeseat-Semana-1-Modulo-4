9f1e9db0d41035deeb56f7b2589c9d090ea02e07
Pedro Alves de Amorim Neto <38478988+ThePetterMaster@users.noreply.github.com>
2020-04-25 18:30:37 -0300

    Add files via upload

---
+++ Configurando NodeMailer/src/config/mail.js
@@ -0,0 +1,13 @@
+export default{
+ 
+    host:'smtp.mailtrap.io',
+    port:' 2525',
+    secure:false,
+    auth: {
+      user: "ddbe931e3a1ae7",
+      pass: "2de31d77107b15"
+    },
+    default:{
+        from:'Equipe GoBarber <noreply@gobarber.com>'
+    }
+};
